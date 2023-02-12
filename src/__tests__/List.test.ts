a52a3afbbb32d123f904fc6bc94f2011a69a6a0e
mauricio.b.mossi <mauricio.b.mossi@gmail.com>
2023-02-11 23:59:55 -0500

    fixing publication issues

--- src/__tests__/List.test.ts
+++ src/__tests__/List.test.ts
@@ -1,5 +1,5 @@
 import {List} from '../index'
 
 test('List initialization test', () => {
-    expect(List(5, (it => 1 + it))).toBe([1,2,3,4,5]);
+    expect(List(5, (it => 1 + it))).toStrictEqual([1,2,3,4,5]);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Kotlin style list creation
 // Props should make a library with Kotlin functions
-export function List<T>(items : number, init : (index : number) => T) : Array<T>{
-    let arr = []
+export function List<T>(items : number, init : (index : number) => T) : T[] {
+    const arr = []
     for(let i = 0; i < items; i++){
         arr[i] = init(i)
     }
