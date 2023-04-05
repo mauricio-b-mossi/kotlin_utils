e3ff9c95749a21968f847eeab62c229f844ed716
mauricio.b.mossi <mauricio.b.mossi@gmail.com>
2023-04-05 00:02:07 -0400

    added JSDoc to functions

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-// Kotlin style list creation.
-export function List<T>(items : number, init : (index : number) => T) : T[] {
+/**
+    * This function returns a list<T> of size n. Each item it created via an init
+    * function, which accepts the index as an argument.
+    * @param {number} size The size of the list.
+    * @param {(number) => T} init The init function for the list. Accepts the index of the list item as an argument.
+    * @return {Array<T>} Returns the list<T>.
+*/
+export function List<T>(size : number, init : (index : number) => T) : T[] {
     const arr = []
-    for(let i = 0; i < items; i++){
+    for(let i = 0; i < size; i++){
         arr[i] = init(i)
     }
     return arr
 }
 
-// Kotlin style repeat function.
+/*
+    * This function perfoms an action n times.
+    * @param {number} times Specifies the amount of times the action will be performed.
+    * @param {() => void} action The action to be performed.
+*/
 export function repeat(times : number, action : () => void) : void{
     for(let i = 0; i < times; i++){
         action()
